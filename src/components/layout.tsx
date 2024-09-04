34979aac8b66605d5b5775350b0e4c4419c0f969
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-04 23:18:43 +0900

    [Add] fontawesome

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,10 @@ import { ReactNode } from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 type LayoutProps = {
   children: ReactNode
 }
