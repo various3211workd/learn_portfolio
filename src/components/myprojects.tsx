3d7724d1edd993a35f54db88fecfec9cc9f4c6dd
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-07 19:29:06 +0900

    [Fix] complete my project files

--- src/components/myprojects.tsx
+++ src/components/myprojects.tsx
@@ -9,40 +9,37 @@ const Projects = () => {
   return (
     <section id="project">
       <div className="pt-20 mx-10">
-        <p className="text-5xl pb-10 text-center underline decoration-[#D8A25E]">
+        <p className="text-5xl pb-10 text-center underline decoration-[#A04747]">
           プロジェクト紹介
         </p>
         <div className="projects-grid">
             {projectData && projectData.map((project) => (
               <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                <div className="mb-8"> 
-                  <div>
-                    <i className="folder-icon text-gray-400 text-4xl">
-                      <FontAwesomeIcon icon={faListCheck} />
-                    </i>
-                  </div>
-                  <div className="grid grid-cols-2">
+                <div className="mb-8">
+                  <div className="grid grid-cols-3">
                     <div className="text-gray-900 font-bold text-xl mb-2">{project.title}</div>
-                    <span className="text-right pr-5 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      test
+                    <span className="col-start-3 inline-block text-center text-white bg-[#D8A25E] rounded-md px-3 py-1 text-sm font-semibold mr-2 mb-2">
+                      {project.dist}
                     </span>
                   </div>
                   <p className="text-gray-700 text-base">{project.description}</p>
                 </div>
-                                
-                <div className="px-6 pt-4 pb-2">
-                  { project.tec.map((tec) => (
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      {tec}
-                    </span>
-                  )) }
+
+                <div className="grid grid-cols-4">
+                  <div className="col-span-3">
+                    { project.tec.map((tec) => (
+                      <span className="inline-block bg-gray-200 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                        {tec}
+                      </span>
+                    )) }
+                  </div>
+
+                  <a href={project.gitHubLink} className='text-right'>
+                    <FontAwesomeIcon icon={faGithub} className="text-gray-900 text-4xl"/>
+                  </a>
+                  </div>
+
                 </div>
-
-                <a href={project.gitHubLink} className='text-right'>
-                  <FontAwesomeIcon icon={faGithub} className="text-gray-900 text-4xl"/>
-                </a>
-
-              </div>
             ))
             }
         </div>
--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -3,7 +3,8 @@ export const projectData = [
       id: 1,
       title: 'アプリ',
       description:
-        '',
+        'test',
+      dist: 'Webアプリ',
       gitHubLink: 'https://github.com/',
       tec: ['Rails', 'Nuxtjs', 'psql'],
     },
