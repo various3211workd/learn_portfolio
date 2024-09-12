fd9975249935d2cbe9cdd1166ac000b74324ab1a
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-13 01:58:59 +0900

    [Fix] project.data pop idea

--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -9,8 +9,17 @@ export const projectData = [
       tec: ['Rails', 'Vuejs', 'PosrgreSQL', 'Docker', 'AWS'],
     },
     {
-      id: 2,
-      title: 'Rustによるコマンドラインツールの作成',
+      id: 4,
+      title: '楽しくアイデア出しができるWebサイト',
+      description:
+        'アイデア出しの方法として「しりとり法」というものがあるという話を聞き、その「しりとり法」を楽しくできないかと思ったことや、「Vuejs」の学習用として作成したWebサイト',
+      dist: 'Webアプリ',
+      gitHubLink: 'https://github.com/various3211workd/pop_idea',
+      tec: ['Vuejs'],
+    },
+    {
+      id: 3,
+      title: 'メモ ＋ CLI',
       description:
         '「メモ」と「TIL(today I Learning)」の2つの言葉を合わせた「mel」というコマンドラインツール。「その日学んだことをすぐにメモに残したかった」という需要と、「Rustというプログラミング言語」への興味があるということから完全に個人用として作成。メモ自体をGithubにpushすることで、簡単に別端末にもメモの閲覧・更新ができる点が魅力。Cでの開発とは違い、Macで開発したものをWindowsとLinuxように簡単位コンパイル点が良かった。',
       dist: 'コマンドラインツール',
@@ -18,8 +27,8 @@ export const projectData = [
       tec: ['Rust', 'ソフトウェア'],
     },
     {
-      id: 3,
-      title: 'セキュリティ特別講師の経験あり',
+      id: 4,
+      title: 'セキュリティ特別講師の経験',
       description:
         'KIIS セキュリティ人材育成プログラムの講師として2022年に登壇の経験あり。「DFIR（デジタルフォレンジックとインシデントレスポンス）の入門と体験」の講義を行い、前半では座学によって具体的にどのような段階を経て攻撃を行うのかという流れなどの説明。後半では実際に擬似的に攻撃されたマシンをフォレンジックし、攻撃の影響範囲の特定や不審なプログラムを探すといったことを行った。https://secure.kiis.or.jp/cybersecurity/images/program2022/flyer.pdf',
       dist: 'Security',
@@ -27,12 +36,12 @@ export const projectData = [
       tec: ['Security', '講師'],
     },
     {
-      id: 4,
-      title: 'セキュリティ特別講師の経験あり',
+      id: 5,
+      title: '',
       description:
-        'KIIS セキュリティ人材育成プログラムの講師として2022年に登壇の経験あり。「DFIR（デジタルフォレンジックとインシデントレスポンス）の入門と体験」の講義を行い、前半では座学によって具体的にどのような段階を経て攻撃を行うのかという流れなどの説明。後半では実際に擬似的に攻撃されたマシンをフォレンジックし、攻撃の影響範囲の特定や不審なプログラムを探すといったことを行いました。https://secure.kiis.or.jp/cybersecurity/images/program2022/flyer.pdf',
-      dist: 'Security',
-      gitHubLink: 'https://kansaidx.kiis.or.jp/event/2022securityprogram/',
-      tec: ['Security', '講師'],
+        '',
+      dist: '',
+      gitHubLink: 'h',
+      tec: [''],
     },
   ];
