477ac69005aebae95900cb83fc7f844eaaa354bd
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-13 01:34:35 +0900

    [Fix] projectdata.js にデータを入れた。

--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -1,11 +1,38 @@
 export const projectData = [
     {
       id: 1,
-      title: 'アプリ',
+      title: '筋トレをする人のためのSNS',
       description:
-        'test',
+        'Rails + Vuejsを使用し、チームで開発を行ったWebアプリケーション。アジャイル手法で開発を進めた。このプロジェクトを進めていて成長できたことは、「タスクを細分化することで役割分担を行い効率良くチーム開発を行えた」ことや、「毎朝の簡易MTGで、受け持ってるタスクの進捗報告や困った箇所についての質問」ができたことがある。また技術的な要素として、RailsをAPIモードで使用し、完全にバックエンドとフロントエンドを分けた点が新しかった。',
       dist: 'Webアプリ',
-      gitHubLink: 'https://github.com/',
-      tec: ['Rails', 'Nuxtjs', 'psql'],
+      gitHubLink: 'https://github.com/various3211workd/musclers',
+      tec: ['Rails', 'Vuejs', 'PosrgreSQL', 'Docker', 'AWS'],
+    },
+    {
+      id: 2,
+      title: 'Rustによるコマンドラインツールの作成',
+      description:
+        '「メモ」と「TIL(today I Learning)」の2つの言葉を合わせた「mel」というコマンドラインツール。「その日学んだことをすぐにメモに残したかった」という需要と、「Rustというプログラミング言語」への興味があるということから完全に個人用として作成。メモ自体をGithubにpushすることで、簡単に別端末にもメモの閲覧・更新ができる点が魅力。Cでの開発とは違い、Macで開発したものをWindowsとLinuxように簡単位コンパイル点が良かった。',
+      dist: 'コマンドラインツール',
+      gitHubLink: 'https://github.com/various3211workd/mel',
+      tec: ['Rust', 'ソフトウェア'],
+    },
+    {
+      id: 3,
+      title: 'セキュリティ特別講師の経験あり',
+      description:
+        'KIIS セキュリティ人材育成プログラムの講師として2022年に登壇の経験あり。「DFIR（デジタルフォレンジックとインシデントレスポンス）の入門と体験」の講義を行い、前半では座学によって具体的にどのような段階を経て攻撃を行うのかという流れなどの説明。後半では実際に擬似的に攻撃されたマシンをフォレンジックし、攻撃の影響範囲の特定や不審なプログラムを探すといったことを行った。https://secure.kiis.or.jp/cybersecurity/images/program2022/flyer.pdf',
+      dist: 'Security',
+      gitHubLink: 'https://kansaidx.kiis.or.jp/event/2022securityprogram/',
+      tec: ['Security', '講師'],
+    },
+    {
+      id: 4,
+      title: 'セキュリティ特別講師の経験あり',
+      description:
+        'KIIS セキュリティ人材育成プログラムの講師として2022年に登壇の経験あり。「DFIR（デジタルフォレンジックとインシデントレスポンス）の入門と体験」の講義を行い、前半では座学によって具体的にどのような段階を経て攻撃を行うのかという流れなどの説明。後半では実際に擬似的に攻撃されたマシンをフォレンジックし、攻撃の影響範囲の特定や不審なプログラムを探すといったことを行いました。https://secure.kiis.or.jp/cybersecurity/images/program2022/flyer.pdf',
+      dist: 'Security',
+      gitHubLink: 'https://kansaidx.kiis.or.jp/event/2022securityprogram/',
+      tec: ['Security', '講師'],
     },
   ];
