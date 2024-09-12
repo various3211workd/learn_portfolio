f64a521ca97a8c69e38793f9dc6fbad075974b4a
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-13 02:47:37 +0900

    [Add] skill.js

---
+++ src/components/skils.js
@@ -0,0 +1,10 @@
+export const skilData = [
+    {
+        id: 1,
+        skill: ['Rails', 'Vue.js', 'Next.js', 'React', 'postgreSQL', 'MySQL', 'Python', 'PHP', 'Java', 'Rust', 'C', 'TypeScript', '', ],
+    },
+    {
+        id: 2,
+        skill: ['', ],
+    },
+]
