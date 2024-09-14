f660a17a6780c61b9d6860025df6b07fcd8e812d
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-14 22:09:27 +0900

    [Fix] skill lists

---
+++ src/components/skilldata.js
@@ -0,0 +1,14 @@
+export const mainSkillData = [
+
+    {
+        id: 1,
+        skill: ['HTML', 'Docker', 'Ruby on Rails', 'Vue.js', 'Next.js', 'React', 'postgreSQL', 'MySQL', 'Python', 'PHP', 'Java', 'Rust', 'C', 'TypeScript', 'CSS', 'Nodejs', 'Linux', 'Git', 'Github', 'Flutter', 'Dart', 'sqlite3', ],
+    },
+];
+
+export const subSkillData = [
+    {
+        id: 2,
+        skill: ['Go', ],
+    },
+]
---
+++ src/components/skils.tsx
@@ -0,0 +1,40 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faShieldHalved } from '@fortawesome/free-solid-svg-icons'
+import { faRust, faPython, faPhp } from '@fortawesome/free-brands-svg-icons';
+
+import { mainSkillData, subSkillData } from './skilldata.js';
+
+const Skills = () => {
+    return (
+      <div className="text-center">
+          <p className="text-5xl pb-10 text-center underline decoration-[#A04747]">スキル</p>
+
+          <p className="text-xl my-5">業務・趣味で成果物を作ったことがある</p>
+          {mainSkillData && mainSkillData.map((skill) => (
+            <div className="flex justify-center items-left">
+              <div className="mx-72">
+                {skill.skill.map((s) => (
+                  <span className="inline-block bg-gray-200 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <p>{s}</p>
+                  </span>
+                ))
+                }
+              </div>
+            </div>
+          ))
+          }
+
+          <p className="text-xl my-5">触ったことがある</p>
+          {subSkillData && subSkillData.map((skill) => (
+            <div>
+            <span className="inline-block bg-gray-200 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              {skill.skill}
+            </span>
+            </div>
+          ))
+          }
+        </div>
+    )
+  }
+  
+  export default Skills;
