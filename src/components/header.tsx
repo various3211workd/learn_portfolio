ae76cf939e66e4f0f97655a10a11cee27f275aec
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-14 23:00:34 +0900

    [Fix] sticky header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,17 +10,14 @@ const Header = () => {
   return (
 
     <ThemeProvider attribute="class" defaultTheme="dark">
-      <header className="sticky top-0 flex flex-row scroll-smooth">
-          
+      <header className="sticky flex flex-row scroll-smooth bg-[#343131] pb-3 bg-opacity-80 top-0 flex flex-row fixed ">
         <a href="#top">
           <FontAwesomeIcon icon={faHouse} className='text-5xl pt-5 pl-5 text-[#A04747] dark:text-[#EEDF7A]'/>
         </a>
 
-        <div className="top-0 flex flex-row fixed left-20">
         <a href="#mynameis" className='text-3xl pt-5 pl-5 text-[#A04747] dark:text-[#EEDF7A] hover:underline decoration-[#A04747]'>MyNameIs</a>
         <a href="#project" className='text-3xl pt-5 pl-5 text-[#A04747] dark:text-[#EEDF7A] hover:underline decoration-[#A04747]'>projects</a>
         <a href="#skills" className='text-3xl pt-5 pl-5 text-[#A04747] dark:text-[#EEDF7A] hover:underline decoration-[#A04747]'>skills</a>
-        </div>
 
         <div className="fixed pt-5 right-5">
           <DarkModeButton />
