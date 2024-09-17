6884dbdfdbff8c7a3c3b1e2cdfbe76adf3e0d7ad
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-18 05:12:28 +0900

    [Fix] footer design

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,8 @@
 
 const Footer = () => {
   return (
-    <footer className="bg-[#343131] bg-opacity-80 flex justify-center">
-        <p className="text-3xl py-5 text-white">footer</p>
+    <footer className="flex justify-center border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+      <p className="text-2xl font-semibold whitespace-nowrap dark:text-white">footer</p>
     </footer>
   )
 }
