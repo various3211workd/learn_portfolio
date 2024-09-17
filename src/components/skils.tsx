69e2b95a2a788826945441c62f05f4fc047db974
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-18 05:08:15 +0900

    [Fix] skill design

--- src/components/skils.tsx
+++ src/components/skils.tsx
@@ -1,21 +1,17 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faShieldHalved } from '@fortawesome/free-solid-svg-icons'
-import { faRust, faPython, faPhp } from '@fortawesome/free-brands-svg-icons';
-
 import { mainSkillData, subSkillData } from './skilldata.js';
 
 const Skills = () => {
     return (
       <section id="skills">
         <div className="text-center pt-20">
-            <p className="text-5xl pb-10 text-center underline decoration-[#A04747]">スキル</p>
+            <p className="text-4xl pb-10 text-center font-semibold whitespace-nowrap underline decoration-[#A04747]">スキル</p>
 
             <p className="text-xl my-5">業務・趣味で成果物を作ったことがある</p>
             {mainSkillData && mainSkillData.map((skill) => (
               <div className="flex justify-center items-left">
-                <div className="mx-72">
+                <div className="md:w-full lg:mx-72">
                   {skill.skill.map((s) => (
-                    <span className="inline-block bg-gray-200 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <span className="inline-block rounded-full px-3 py-1 mr-2 mb-2 font-medium border bg-white border-gray-300">
                       <p>{s}</p>
                     </span>
                   ))
@@ -27,14 +23,16 @@ const Skills = () => {
 
             <p className="text-xl my-5">触ったことがある</p>
             {subSkillData && subSkillData.map((skill) => (
-              <div>
-              <span className="inline-block bg-gray-200 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                {skill.skill}
-              </span>
+              <div className="flex justify-center items-left">
+                <div className="md:w-full lg:mx-72">
+                  <span className="inline-block rounded-full px-3 py-1 mr-2 mb-2 font-medium border bg-white border-gray-300">
+                    <p>{skill.skill}</p>
+                  </span>
+                </div>
               </div>
             ))
             }
-          </div>
+          </div>        
         </section>
     )
   }
