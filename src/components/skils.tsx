1a75f70798a1538c23e5ecc0a2ef262dd075540b
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-19 03:43:25 +0900

    [Fix] skill design -> darkmode

--- src/components/skils.tsx
+++ src/components/skils.tsx
@@ -1,40 +1,41 @@
 import { mainSkillData, subSkillData } from './skilldata.js';
 
 const Skills = () => {
-    return (
-      <section id="skills">
-        <div className="text-center pt-20">
-            <p className="text-4xl pb-10 text-center font-semibold whitespace-nowrap underline decoration-[#A04747]">スキル</p>
+  return (
+    <section id="skills">
+      <div className="text-center pt-20">
+        <p className="text-4xl pb-10 text-center font-semibold whitespace-nowrap underline decoration-[#A04747]">
+          スキル
+        </p>
 
-            <p className="text-xl my-5">業務・趣味で成果物を作ったことがある</p>
-            {mainSkillData && mainSkillData.map((skill) => (
-              <div className="flex justify-center items-left">
-                <div className="md:w-full lg:mx-72">
-                  {skill.skill.map((s) => (
-                    <span className="inline-block rounded-full px-3 py-1 mr-2 mb-2 font-medium border bg-white border-gray-300">
-                      <p>{s}</p>
-                    </span>
-                  ))
-                  }
-                </div>
-              </div>
-            ))
-            }
-
-            <p className="text-xl my-5">触ったことがある</p>
-            {subSkillData && subSkillData.map((skill) => (
-              <div className="flex justify-center items-left">
-                <div className="md:w-full lg:mx-72">
-                  <span className="inline-block rounded-full px-3 py-1 mr-2 mb-2 font-medium border bg-white border-gray-300">
-                    <p>{skill.skill}</p>
+        <p className="text-xl my-5">業務・趣味で成果物を作ったことがある</p>
+        {mainSkillData &&
+          mainSkillData.map((skill) => (
+            <div className="flex justify-center items-left">
+              <div className="md:w-full lg:mx-72">
+                {skill.skill.map((s) => (
+                  <span className="inline-block rounded-full px-3 py-1 mr-2 mb-2 font-medium border bg-white dark:bg-gray-800 border-gray-300">
+                    <p>{s}</p>
                   </span>
-                </div>
+                ))}
               </div>
-            ))
-            }
-          </div>        
-        </section>
-    )
-  }
-  
-  export default Skills;
+            </div>
+          ))}
+
+        <p className="text-xl my-5">触ったことがある</p>
+        {subSkillData &&
+          subSkillData.map((skill) => (
+            <div className="flex justify-center items-left">
+              <div className="md:w-full lg:mx-72">
+                <span className="inline-block rounded-full px-3 py-1 mr-2 mb-2 font-medium border bg-white dark:bg-gray-800 border-gray-300">
+                  <p>{skill.skill}</p>
+                </span>
+              </div>
+            </div>
+          ))}
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
