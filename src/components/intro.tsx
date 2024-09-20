99f93d320608d2421af35d84e1df19ec962a072f
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-21 05:23:57 +0900

    [Fix] intro design

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,13 +3,20 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const Intro = () => {
   return (
-    <div className="flex justify-center h-screen w-screen overflow-scroll">
-      <div className="grid content-center pb-32 duration-75 animate-tracking-in-expand">
-        <p className="md:text-6xl text-4xl font-semibold">まいぽーとふぉりお</p>
-      </div>
-      <div className="flex justify-center flex-col absolute bottom-20 text-2xl ease-in duration-300 hover:text-3xl cursor-pointer">
-        <FontAwesomeIcon icon={faChevronDown} />
-        <p>scroll</p>
+    <div className="min-h-[45vh] bg-center bg-cover bg-opacity-75">
+      <div className="flex justify-center h-screen w-screen overflow-scroll">
+        <div className="grid content-center pb-32 duration-75 animate-tracking-in-expand md:text-8xl text-4xl relative">
+          <p className="font-serif text-center text-sky-500 opacity-50 absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            PORTFOLIO
+          </p>
+          <p className="font-semibold z-0 text-slate-800 dark:text-white">
+            ぽーとふぉりお
+          </p>
+        </div>
+        <div className="flex justify-center flex-col absolute bottom-20 text-2xl animate-bounce duration-300 hover:font-bold">
+          <FontAwesomeIcon icon={faChevronDown} />
+          <p>scroll</p>
+        </div>
       </div>
     </div>
   );
