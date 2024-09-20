54d9d11fdaa6d4d0cf34e7e45015c8c609a43373
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-21 05:22:25 +0900

    [Fix] remove header icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,11 +13,6 @@ const Header = () => {
           href="#top"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             ぽーとふぉりお
           </span>
