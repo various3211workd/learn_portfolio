9164413a948a57a78170a1a3a6ec634fd33446e6
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-23 02:13:47 +0900

    [Fix] slide design

--- src/components/slide.tsx
+++ src/components/slide.tsx
@@ -1,8 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSlideshare } from '@fortawesome/free-brands-svg-icons';
+
 const Slide = () => {
   return (
     <section id="slide">
       <div className="text-center pt-20">
-        <p>test</p>
+        <p className="text-4xl pb-10 text-center font-semibold whitespace-nowrap underline decoration-[#A04747]">
+          <FontAwesomeIcon icon={faSlideshare} className="pr-2" />
+          Slide
+        </p>
+        <p className="text-xl my-5">勉強会などで使用したスライド</p>
       </div>
     </section>
   );
