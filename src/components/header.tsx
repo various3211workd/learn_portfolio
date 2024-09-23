185bd0698d4a911ea0202636a3d7cbf7283ac7b8
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-24 04:51:41 +0900

    [Add] sidebar component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,18 @@
 'use client';
 
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 import { DarkModeButton } from './darkmodebutton';
+import Sidebar from './sidebar';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -17,23 +24,25 @@ const Header = () => {
             ぽーとふぉりお
           </span>
         </a>
+
+        <Sidebar isOpen={isOpen} toggle={toggle} />
+
         <button
-          data-collapse-toggle="navbar-solid-bg"
           type="button"
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-solid-bg"
-          aria-expanded="false"
+          onClick={toggle}
         >
-          <FontAwesomeIcon icon={faBars} className="text-gray-900 text-auto" />
+          <FontAwesomeIcon icon={faBars} className="text-auto" />
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+
+        <div className="hidden w-full md:block md:w-auto">
           <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
             <li>
               <a
                 href="#mynameis"
                 className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                introduction
+                Introduction
               </a>
             </li>
             <li>
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,60 @@
+import { DarkModeButton } from './darkmodebutton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+const Sidebar = ({
+  isOpen,
+  toggle,
+}: {
+  isOpen: boolean;
+  toggle: () => void;
+}): JSX.Element => {
+  return (
+    <>
+      <div
+        className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 sidebar-container fixed w-full h-full overflow-hidden justify-center bg-white grid pt-[120px] left-0 z-10"
+        style={{
+          opacity: `${isOpen ? '1' : '0'}`,
+          top: ` ${isOpen ? '0' : '-100%'}`,
+        }}
+      >
+        <button className="absolute right-0 p-5" onClick={toggle}>
+          <FontAwesomeIcon icon={faXmark} className="text-auto" />
+        </button>
+
+        <ul className="sidebar-nav text-center leading-relaxed text-xl">
+          <li>
+            <a href="#top" className="block py-2" onClick={toggle}>
+              TOP
+            </a>
+          </li>
+          <li>
+            <a href="#mynameis" className="block py-2" onClick={toggle}>
+              introduction
+            </a>
+          </li>
+          <li>
+            <a href="#project" className="block py-2" onClick={toggle}>
+              Project
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="block py-2" onClick={toggle}>
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#slide" className="block py-2 " onClick={toggle}>
+              Slide
+            </a>
+          </li>
+          <li>
+            <DarkModeButton />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
