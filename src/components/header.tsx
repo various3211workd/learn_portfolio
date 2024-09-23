a3ec5daa3d8e065492ef800efe682214efabc30b
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-09-24 04:53:17 +0900

    [Fix] headerのtabをlist化して追加・削除しやすくした

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,6 +13,13 @@ const Header = () => {
     setIsOpen(!isOpen);
   };
 
+  const nablist = [
+    { id: '#mynameis', nav: 'Introduction' },
+    { id: '#project', nav: 'Project' },
+    { id: '#skills', nav: 'Skills' },
+    { id: '#slide', nav: 'Slide' },
+  ];
+
   return (
     <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -37,38 +44,16 @@ const Header = () => {
 
         <div className="hidden w-full md:block md:w-auto">
           <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-            <li>
-              <a
-                href="#mynameis"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Introduction
-              </a>
-            </li>
-            <li>
-              <a
-                href="#project"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Project
-              </a>
-            </li>
-            <li>
-              <a
-                href="#skills"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Skills
-              </a>
-            </li>
-            <li>
-              <a
-                href="#slide"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Slide
-              </a>
-            </li>
+            {nablist.map((nav: any, index: any) => (
+              <li key={index}>
+                <a
+                  href={nav.id}
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  {nav.nav}
+                </a>
+              </li>
+            ))}
             <li>
               <DarkModeButton />
             </li>
