9615c7cf2f443072329c0ad1ea147a3fb225460c
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2024-10-19 20:31:35 +0900

    [Change] 不要なLayout.tsxを削除して統合

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
-import Layout from '../components/layout';
 import Skils from '../components/skils';
 import Myprojects from '../components/introfuction';
 import Intro from '../components/intro';
 import MyNameIs from '../components/mynameis';
 import Slide from '@/components/slide';
+import Header from '../components/header';
+import Footer from '../components/footer';
 
 export default function Home() {
   return (
-    <Layout>
+    <>
+      <Header />
+
       <Intro />
-
       <MyNameIs />
-
       <Myprojects />
       <Skils />
       <Slide />
-    </Layout>
+
+      <Footer />
+    </>
   );
 }
